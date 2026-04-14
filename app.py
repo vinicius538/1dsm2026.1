f467eb63574768e78be2af6f5410d601598d811e
Vinicius de Souza <vinicius.souza110208@gmail.com>
2026-04-14 20:02:03 +0000

    feat: adiciona o ambiente flask

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/sobrenos")
+def sobrenos():
+    return render_template("sobrenos.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
